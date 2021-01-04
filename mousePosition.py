548f2990a13a0f55fd2074b121f328ba725591da
voyager2005 <76808676+voyager2005@users.noreply.github.com>
2021-01-04 18:48:19 +0530

    Update mousePosition.py

--- mousePosition.py
+++ mousePosition.py
@@ -5,11 +5,11 @@ def motion(event):
     x = event.x - 386
     y = event.y
 
-    if y > 327:
-        y = y - 327
-    elif y < 327:
-        y = 327 - y
-    elif y == 327:
+    if y > 326:
+        y = -(y - 326)
+    elif y < 326:
+        y = 326 - y
+    elif y == 326:
         y = 0
     print('{}, {}'.format(x, y))
 
