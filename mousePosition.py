114fb44951e4259f3ed8379a672d2e08a60c2e87
voyager2005 <76808676+voyager2005@users.noreply.github.com>
2021-01-04 18:37:35 +0530

    Create mousePosition.py
    Find the EXACT LOCATION of your mouse real time while running code

---
+++ mousePosition.py
@@ -0,0 +1,18 @@
+import turtle
+
+
+def motion(event):
+    x = event.x - 386
+    y = event.y
+
+    if y > 327:
+        y = y - 327
+    elif y < 327:
+        y = 327 - y
+    print('{}, {}'.format(x, y))
+
+
+canvas = turtle.getcanvas()
+canvas.bind('<Motion>', motion)
+
+turtle.done()
